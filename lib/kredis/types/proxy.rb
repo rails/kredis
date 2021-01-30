28ca32a13a0a3b8584e708049e3ffac0712836fc
David Heinemeier Hansson <david@loudthinking.com>
2021-01-30 19:56:46 +0100

    Less noisy logging

--- lib/kredis/types/proxy.rb
+++ lib/kredis/types/proxy.rb
@@ -8,7 +8,7 @@ class Kredis::Types::Proxy
   end
 
   def method_missing(method, *args, **kwargs)
-    Kredis.logger&.debug "[Kredis] #{method} #{@key} #{args.inspect} #{kwargs.inspect}"
+    Kredis.logger&.debug "[Kredis] #{method} #{@key} #{args.inspect if args.compact_blank.any?} #{kwargs.inspect if kwargs.compact_blank.any?}".chomp
     @redis.public_send method, @key, *args, **kwargs
   end
 end
