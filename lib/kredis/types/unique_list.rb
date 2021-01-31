27a3d5c967198c4c602d79ddd46dd0560653cf7b
Kasper Timm Hansen <kaspth@gmail.com>
2021-01-31 19:12:09 +0100

    Revert "Trial out a common trimming abstraction"
    This reverts commit ebc94d91bb1f6ec4d2c56dcd8d26ebbe97739136.

--- lib/kredis/types/unique_list.rb
+++ lib/kredis/types/unique_list.rb
@@ -3,24 +3,19 @@ class Kredis::Types::UniqueList < Kredis::Types::List
   attr_accessor :limit
 
   def prepend(elements)
-    with_trimming_of(elements, to: limit_index) { super }
+    multi do
+      remove elements
+      super
+      ltrim 0, (limit - 1) if limit
+    end if Array(elements).any?
   end
 
   def append(elements)
-    with_trimming_of(elements, from: limit_index) { super }
+    multi do
+      remove elements
+      super
+      ltrim (limit - 1), -1 if limit
+    end if Array(elements).any?
   end
   alias << append
-
-  private
-    def with_trimming_of(elements, from: 0, to: -1)
-      multi do
-        remove elements
-        yield
-        ltrim from, to if limit
-      end if Array(elements).any?
-    end
-
-    def limit_index
-      limit - 1 if limit
-    end
 end
