7ae166ccb8c8a0d7ff7f4968af9437570fc490b4
Kasper Timm Hansen <kaspth@gmail.com>
2021-01-31 08:56:36 +0100

    Spike out a slot concept for distributed locks

---
+++ lib/kredis/types/slot.rb
@@ -0,0 +1,34 @@
+# For distributed locks where you want a `max` of available units of work to happen concurrently.
+class Kredis::Types::Slot < Kredis::Types::Proxy
+  attr_accessor :max
+
+  def initialize(*)
+    super
+    @original_key = key
+  end
+
+  def acquire
+    @redis.exists(*slot_keys).find do |key|
+      if @redis.set key, slot_id, nx: true
+        @key = key
+      end
+    end
+  end
+
+  def assigned?
+    get == slot_id
+  end
+
+  def waive
+    del
+  end
+
+  private
+    def slot_keys
+      max.times { |index| "#{@original_key}/slot/#{index}" }
+    end
+
+    def slot_id
+      (rand(1_000) % max).to_s
+    end
+end
