ebc94d91bb1f6ec4d2c56dcd8d26ebbe97739136
Kasper Timm Hansen <kaspth@gmail.com>
2021-01-31 11:17:30 +0100

    Trial out a common trimming abstraction

--- lib/kredis/types/unique_list.rb
+++ lib/kredis/types/unique_list.rb
@@ -3,18 +3,23 @@ class Kredis::Types::UniqueList < Kredis::Types::List
   attr_accessor :limit
 
   def prepend(elements)
-    multi do
-      remove elements
-      super
-      ltrim 0, (limit - 1) if limit
-    end if Array(elements).any?
+    with_trimming_of(elements, to: limit_index) { super }
   end
 
   def append(elements)
-    multi do
-      remove elements
-      super
-      ltrim (limit - 1), -1 if limit
-    end if Array(elements).any?
+    with_trimming_of(elements, from: limit_index) { super }
   end
+
+  private
+    def with_trimming_of(elements, from: 0, to: -1)
+      multi do
+        remove elements
+        yield
+        ltrim from, to if limit
+      end if Array(elements).any?
+    end
+
+    def limit_index
+      limit - 1 if limit
+    end
 end
