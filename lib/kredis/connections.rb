396a306256825c0b4b97c201f0e8db6f2c186f89
David Heinemeier Hansson <david@loudthinking.com>
2021-01-31 14:47:46 +0100

    Better logging for connections

--- lib/kredis/connections.rb
+++ lib/kredis/connections.rb
@@ -5,11 +5,14 @@ module Kredis::Connections
   mattr_accessor :configurator
 
   def configured_for(name)
-    connections[name] ||= Redis.new configurator.config_for("redis/#{name}")
+    connections[name] ||= begin
+      logger&.info "[Kredis] Connected to #{name}"
+      Redis.new configurator.config_for("redis/#{name}")
+    end
   end
 
   def clear_all
-    logger&.info "[Kredis] Cleared all connections"
+    logger&.info "[Kredis] Connections all cleared"
     connections.each_value(&:flushdb)
   end
 end
