7f620145e90a3ed470f8ddd1c2f6e88bdcd65236
Kasper Timm Hansen <kaspth@gmail.com>
2021-02-01 07:54:07 +0100

    Put setter above getter to fit with other types

--- lib/kredis/types/datetime.rb
+++ lib/kredis/types/datetime.rb
@@ -1,11 +1,11 @@
 class Kredis::Types::Datetime < Kredis::Types::String
+  def value=(value)
+    super value.to_f
+  end
+  
   def value
     if value = get&.to_i
       Time.at value
     end
-  end
-
-  def value=(value)
-    super value.to_f
   end
 end
