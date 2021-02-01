b7433795f15b1cab8bc67f3f7b833d5ab5d4e182
Kasper Timm Hansen <kaspth@gmail.com>
2021-02-01 15:36:17 +0100

    Kredis automatically eager loads by default

--- lib/kredis/railtie.rb
+++ lib/kredis/railtie.rb
@@ -3,7 +3,6 @@ require "rails/railtie"
 module Kredis
   class Railtie < ::Rails::Railtie
     config.kredis = ActiveSupport::OrderedOptions.new
-    config.eager_load_namespaces << Kredis
 
     initializer "kredis.testing" do
       ActiveSupport.on_load(:active_support_test_case) do
