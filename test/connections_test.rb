9d6bf7db9c97a26bd71017cb505304bde2672b88
Kasper Timm Hansen <kaspth@gmail.com>
2021-02-11 14:30:23 +0100

    Add failing test for c68547f7d3a8387c9740b1de14f0d60448c68b14
    Laziness wins again! Confirmed the fix worked in app, and now I have
    the motivation to add the test 😂

--- test/connections_test.rb
+++ test/connections_test.rb
@@ -1,6 +1,8 @@
 require "test_helper"
 
 class ConnectionsTest < ActiveSupport::TestCase
+  teardown { Kredis.namespace = nil }
+
   test "clear all" do
     list = Kredis.list "mylist"
     list.append "one"
@@ -8,5 +10,18 @@ class ConnectionsTest < ActiveSupport::TestCase
 
     Kredis.clear_all
     assert_equal [], list.elements
+  end
+
+  test "clear all with namespace" do
+    Kredis.configured_for(:shared).set "mykey", "don't remove me"
+
+    Kredis.namespace = "test-1"
+    integer = Kredis.integer "myinteger"
+    integer.value = 1
+
+    Kredis.clear_all
+
+    assert_nil integer.value
+    assert_equal "don't remove me", Kredis.configured_for(:shared).get("mykey")
   end
 end
