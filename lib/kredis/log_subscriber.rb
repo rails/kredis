e0e4a4757ec4a5854a5ab0294a25ef5f7c7ee2ae
Kasper Timm Hansen <kaspth@gmail.com>
2021-07-26 23:10:21 +0200

    Clarify variable purpose with a rename

--- lib/kredis/log_subscriber.rb
+++ lib/kredis/log_subscriber.rb
@@ -2,18 +2,18 @@ require "active_support/log_subscriber"
 
 class Kredis::LogSubscriber < ActiveSupport::LogSubscriber
   def proxy(event)
-    name = color("Kredis #{event.payload[:type]} (#{event.duration.round(1)}ms)", YELLOW, true)
-    debug "  #{name}  #{color(event.payload[:message], YELLOW, true)}"
+    prefix = color("Kredis #{event.payload[:type]} (#{event.duration.round(1)}ms)", YELLOW, true)
+    debug "  #{prefix}  #{color(event.payload[:message], YELLOW, true)}"
   end
 
   def migration(event)
-    name = color("Kredis Migration (#{event.duration.round(1)}ms)", YELLOW, true)
-    debug "  #{name}  #{color(event.payload[:message], YELLOW, true)}"
+    prefix = color("Kredis Migration (#{event.duration.round(1)}ms)", YELLOW, true)
+    debug "  #{prefix}  #{color(event.payload[:message], YELLOW, true)}"
   end
 
   def meta(event)
-    name = color("Kredis (#{event.duration.round(1)}ms)", MAGENTA, true)
-    info "  #{name}  #{color(event.payload[:message], MAGENTA, true)}"
+    prefix = color("Kredis (#{event.duration.round(1)}ms)", MAGENTA, true)
+    info "  #{prefix}  #{color(event.payload[:message], MAGENTA, true)}"
   end
 end
 
