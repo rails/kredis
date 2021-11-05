b22ab08c0b55c3fb3ed83fa26b2eda53ecbe768c
Val Aleksenko <val@basecamp.com>
2021-11-05 14:05:22 -0400

    Add support for the NX (not exists) option to flags

--- test/types/flag_test.rb
+++ test/types/flag_test.rb
@@ -23,4 +23,19 @@ class FlagTest < ActiveSupport::TestCase
     sleep 0.6.seconds
     assert_not @flag.marked?
   end
+
+  test "mark unless exists" do
+    assert @flag.mark(expires_in: 1.second, unless_exists: true)
+    assert @flag.mark(expires_in: 1.second)
+    assert @flag.mark(expires_in: 1.second, unless_exists: false)
+    assert_not @flag.mark(expires_in: 10.seconds, unless_exists: true)
+
+    assert @flag.marked?
+
+    sleep 0.5.seconds
+    assert @flag.marked?
+
+    sleep 0.6.seconds
+    assert_not @flag.marked?
+  end
 end
