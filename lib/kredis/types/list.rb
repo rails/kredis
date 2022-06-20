d3ed496e723ffa2da949d8ea3dc511d7e6c9c800
Matt Swanson <matt@arrows.to>
2022-06-20 14:49:00 -0400

    Extract method

--- lib/kredis/types/list.rb
+++ lib/kredis/types/list.rb
@@ -10,21 +10,26 @@ class Kredis::Types::List < Kredis::Types::Proxying
 
   def remove(*elements, pipeline: nil)
     types_to_strings(elements, typed).each { |element| (pipeline || proxy).lrem 0, element }
-    (pipeline || proxy).expire expires_in.to_i if expires_in
+    refresh_expiration(pipeline || proxy)
   end
 
   def prepend(*elements, pipeline: nil)
     (pipeline || proxy).lpush types_to_strings(elements, typed) if elements.flatten.any?
-    (pipeline || proxy).expire expires_in.to_i if expires_in
+    refresh_expiration(pipeline || proxy)
   end
 
   def append(*elements, pipeline: nil)
     (pipeline || proxy).rpush types_to_strings(elements, typed) if elements.flatten.any?
-    (pipeline || proxy).expire expires_in.to_i if expires_in
+    refresh_expiration(pipeline || proxy)
   end
   alias << append
 
   def clear
     del
   end
+
+  private
+    def refresh_expiration(target)
+      target.expire expires_in.to_i if expires_in
+    end
 end
