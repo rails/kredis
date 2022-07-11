868b5f1ceff40dc54c6076eefeec3ac0c563b72b
Lewis Buckley <lewis@basecamp.com>
2022-07-11 09:30:24 +0100

    Refactor fail safe to express intentions more clearly

--- lib/kredis/types/proxy/failsafe.rb
+++ lib/kredis/types/proxy/failsafe.rb
@@ -7,19 +7,10 @@ module Kredis::Types::Proxy::Failsafe
 
   def failsafe(returning: nil, &block)
     if fail_safe_enabled? && !fail_safe_suppressed?
-      suppress_fail_safe_with(returning: returning, &block)
+      rescue_redis_errors_with(returning: returning, &block)
     else
       yield
     end
-  end
-
-  def suppress_fail_safe_with(returning: nil)
-    old_fail_safe_suppressed, @fail_safe_suppressed = @fail_safe_suppressed, true
-    yield
-  rescue Redis::BaseError
-    returning
-  ensure
-    @fail_safe_suppressed = old_fail_safe_suppressed
   end
 
   private
@@ -29,5 +20,14 @@ module Kredis::Types::Proxy::Failsafe
 
     def fail_safe_suppressed?
       @fail_safe_suppressed
+    end
+
+    def rescue_redis_errors_with(returning: nil)
+      old_fail_safe_suppressed, @fail_safe_suppressed = @fail_safe_suppressed, true
+      yield
+    rescue Redis::BaseError
+      returning
+    ensure
+      @fail_safe_suppressed = old_fail_safe_suppressed
     end
 end
