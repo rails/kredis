42df3fdacc088b0138487d21c6982f1d9e3d0837
David Heinemeier Hansson <david@hey.com>
2023-12-26 21:19:20 -0800

    Use a default config if shared.yml isn't available

--- test/connections_test.rb
+++ test/connections_test.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 require "test_helper"
+require "yaml"
 
 class ConnectionsTest < ActiveSupport::TestCase
+  setup { Kredis.connections = {} }
   teardown { Kredis.namespace = nil }
 
   test "clear all" do
@@ -25,5 +27,26 @@ class ConnectionsTest < ActiveSupport::TestCase
 
     assert_nil integer.value
     assert_equal "don't remove me", Kredis.configured_for(:shared).get("mykey")
+  end
+
+  test "config from file" do
+    fixture_config = YAML.load_file(Pathname.new(Dir.pwd).join("test/fixtures/config/redis/shared.yml"))["test"].symbolize_keys
+
+    Kredis.configurator.stub(:config_for, fixture_config) do
+      Kredis.configurator.stub(:root, Pathname.new(Dir.pwd).join("test/fixtures")) do
+        assert_match %r|redis://127.0.0.1:6379/4|, Kredis.redis.inspect
+      end
+    end
+  end
+
+  test "default config in env" do
+    ENV["REDIS_URL"] = "redis://127.0.0.1:6379/3"
+    assert_match %r|redis://127.0.0.1:6379/3|, Kredis.redis.inspect
+  ensure
+    ENV.delete("REDIS_URL")
+  end
+
+  test "default config without env" do
+    assert_match %r|redis://127.0.0.1:6379/0|, Kredis.redis.inspect
   end
 end
