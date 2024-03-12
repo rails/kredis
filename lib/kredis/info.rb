25ab41b4cd63748feeb8fcfacadb37f0c1d0f02f
Koo <heka1024@gmail.com>
2024-03-13 00:47:33 +0900

    Apply review
    Co-authored-by: Jeremy Daer <jeremydaer@gmail.com>

--- lib/kredis/info.rb
+++ lib/kredis/info.rb
@@ -2,13 +2,6 @@
 
 module Kredis::Info
   def redis_version
-    redis_versions.first
-  end
-
-  def redis_versions
-    Array.wrap(Kredis.redis.info("server")).tap do |versions|
-      versions.map! { |v| v["redis_version"] }
-      versions.map! { |v| Gem::Version.new(v) }
-    end
+    Gem::Version.new Kredis.redis.info("server")["redis_version"]
   end
 end
